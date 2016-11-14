d6883899600ef763d7dadb0a8e7c9799cb7291ad
Bryan Pedlar <pedlar.bryan@gmail.com>
2016-11-13 19:01:34 -0500

    no more hard coded reference to table name

--- handler.py
+++ handler.py
@@ -1,9 +1,14 @@
 import logging
 import boto3
+import os
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from datetime import datetime
+from os.path import join, dirname
+from dotenv import load_dotenv
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -19,7 +24,7 @@ publish_parser.add_argument('AccountId', required=True,
 class HelloWorld(Resource):
     def __init__(self, **kwargs):
         self.dynamodb = kwargs["dynamodb"]
-        self.table = self.dynamodb.Table('devBiStream')
+        self.table = self.dynamodb.Table(os.environ.get("STREAM_NAME"))
 
     def post(self):
         args = publish_parser.parse_args()
