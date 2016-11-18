560f322d15778d4ffbe8ebf615b245baa1a073ce
bryan pedlar <bryan@thrivehive.com>
2016-11-17 22:39:10 -0500

    purchase, map, call, record phones

--- flask_resources/phone_number.py
+++ flask_resources/phone_number.py
@@ -5,17 +5,14 @@ from flask import request, make_response
 import boto3
 import os
 import logging
-
+import json
+from dateutil.parser import parse
 
 client = TwilioRestClient()
 
 purchase_parser = reqparse.RequestParser()
 purchase_parser.add_argument('purchase', required=True,
                              help='Number to purchase is required')
-
-release_parser = reqparse.RequestParser()
-release_parser.add_argument('number', required=True,
-                            help='Number to purchase is required')
 
 phone_number_list_fields = {
     "friendly_name": fields.String(),
@@ -33,6 +30,9 @@ dynamodb = boto3.resource("dynamodb")
 phone_mapping_table = dynamodb.Table(
     os.environ.get("PHONE_MAPPING_TABLE_NAME"))
 
+phone_record_table = dynamodb.Table(
+    os.environ.get("PHONE_RECORD_TABLE_NAME"))
+
 
 class PhoneNumber(Resource):
     def __init__(self, **kwargs):
@@ -43,11 +43,31 @@ class PhoneNumber(Resource):
         return client.phone_numbers.list()
 
     def put(self, phone_number):
-        print os.environ.get("PHONE_MAPPING_TABLE_NAME")
-        phone_mapping_table.put_item(Item={
-            "PhoneNumber": phone_number,
-            "MappedNumber": request.get_json()["map_to"]
-        })
+        phone_mapping_table.update_item(
+            Key={
+                'PhoneNumber': phone_number
+            },
+            UpdateExpression="set MappedNumber = :r",
+            ExpressionAttributeValues={
+                ':r': request.get_json()["map_to"]
+            },
+            ReturnValues="UPDATED_NEW")
+        return '', 204
+
+    def delete(self, phone_number):
+        response = phone_mapping_table.get_item(
+            Key={
+                'PhoneNumber': phone_number
+            }
+        )
+
+        phone_map = response['Item']['Sid']
+        client.phone_number.delete(phone_map)
+
+        phone_mapping_table.delete_item(
+            Key={
+                'PhoneNumber': phone_number,
+            })
         return '', 204
 
 
@@ -70,9 +90,21 @@ class PhoneNumberList(Resource):
         number_to_purchase = args['purchase']
         number = client.phone_numbers.purchase(
             friendly_name="My Company Line",
-            voice_url=os.environ.get('VOICE_RESPONSE_URL'),
             phone_number=number_to_purchase,
-            voice_method="GET"
+            voice_url=os.environ.get('VOICE_RESPONSE_URL'),
+            voice_method="GET",
+            status_callback=os.environ.get("STATUS_CALLBACK_URL"),
+            status_method="POST"
+        )
+        phone_mapping_table.update_item(
+            Key={
+                'PhoneNumber': number.phone_number
+            },
+            UpdateExpression="set Sid = :r",
+            ExpressionAttributeValues={
+                ':r': number.sid
+            },
+            ReturnValues="UPDATED_NEW"
         )
         return number
 
@@ -88,10 +120,22 @@ class PhoneNumberRespond(Resource):
                 'PhoneNumber': to_number
             }
         )
-        print response
         item = response['Item']
         resp = twilio.twiml.Response()
         resp.dial(item['MappedNumber'])
         # we do this to force the xml response
         # twilio doesn't send accept headers
         return make_response(str(resp), 200)
+
+
+class PhoneNumberRecord(Resource):
+    def __init__(self, **kwargs):
+        pass
+
+    def post(self):
+        phone_record_table.put_item(Item={
+            "PhoneNumber": request.form['To'],
+            "Timestamp": parse(request.form["Timestamp"]).strftime("%Y-%m-%d %H:%M:%S"),
+            "Body": json.dumps(request.form)
+        })
+        return '', 204
--- handler.py
+++ handler.py
@@ -3,7 +3,12 @@ import logging
 import settings
 from flask import Flask
 from flask_restful import Api
-from flask_resources.phone_number import PhoneNumber, PhoneNumberList, PhoneNumberRespond
+from flask_resources.phone_number import (
+    PhoneNumber,
+    PhoneNumberList,
+    PhoneNumberRespond,
+    PhoneNumberRecord
+)
 
 
 logger = logging.getLogger()
@@ -12,10 +17,12 @@ logger.setLevel(logging.INFO)
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(PhoneNumber, '/phone_number', '/phone_number/<string:phone_number>')
+api.add_resource(PhoneNumber, '/phone_number',
+                 '/phone_number/<string:phone_number>')
 api.add_resource(PhoneNumberList, '/phone_number/list')
 api.add_resource(PhoneNumberRespond, '/phone_number/respond')
+api.add_resource(PhoneNumberRecord, '/phone_number/record')
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
